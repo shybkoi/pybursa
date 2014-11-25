cc8798da7cdaaef6074063285f3cdf1110fd1bf1
shybkoi <s.shybkoi@gmail.com>
2014-11-25 19:46:53 +0200

    HW#7

--- pybursa/urls.py
+++ pybursa/urls.py
@@ -8,5 +8,7 @@ from django.contrib import admin
 urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
     url(r'^$', TemplateView.as_view(template_name='index.html'), name='home'),
+    url(r'^products/$', TemplateView.as_view(template_name='products.html'), name='products'),
+    url(r'^product/$', TemplateView.as_view(template_name='product.html'), name='product'),
     url(r'^admin/', include(admin.site.urls)),
 )
